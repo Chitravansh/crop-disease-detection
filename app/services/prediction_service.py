32a52f1193e52d6627578b29e5787d912a3a3bc2
Deependra <bhattdeependra01@gmail.com>
2026-03-19 11:22:03 +0530

    Updated prediction service for compatibility

--- app/services/prediction_service.py
+++ app/services/prediction_service.py
@@ -1,7 +1,7 @@
  
 import numpy as np
 from keras.models import load_model
-from keras.preprocessing import image
+from keras.preprocessing.image import load_img, img_to_array
 # from keras.utils import load_img, img_to_array
 
 
@@ -15,17 +15,21 @@ print(" ** Model Loaded **")
 
 
 def model_predict(img_path):
+    try:
+        img = load_img(img_path, target_size=(224, 224))
 
-    img = image.load_img(img_path, target_size=(224, 224))
+        x = img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = x / 255
 
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = x / 255
+        preds = model.predict(x)
+        d = preds.flatten()
+        index = np.argmax(d)
+        class_name = disease_labels[index].split("___")
+        confidence = float(d[index]) * 100
 
-    preds = model.predict(x)
-    d = preds.flatten()
-    index = np.argmax(d)
-    class_name = disease_labels[index].split("___")
-    confidence = float(d[index]) * 100
-
-    return class_name, confidence
+        return class_name, confidence
+    
+    except Exception as e:
+        print("Prediction Error: ", str(e))
+        return ["Unknown", "Error"], 0.0
